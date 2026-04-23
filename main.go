7e11b6853c97804456443f906ab54b461049c86d
Tom Lebreux <tomleb@users.noreply.github.com>
2026-04-22 21:40:58 -0400

    Add webhook and norman to playground deps

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/tomleb/frameworks-automation-norman/pkg/norman"
 	"github.com/tomleb/frameworks-automation-steve/pkg/steve"
+	"github.com/tomleb/frameworks-automation-webhook/pkg/webhook"
 	"github.com/tomleb/frameworks-automation-wrangler/pkg/wrangler"
 )
 
 func main() {
 	fmt.Println(wrangler.Hello())
 	fmt.Println(steve.Hello())
+	fmt.Println(norman.Hello())
+	fmt.Println(webhook.Hello())
 }
